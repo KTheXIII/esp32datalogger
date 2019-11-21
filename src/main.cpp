4ad62519da0b861ecd1f1beaf3135dd9f177f1e5
KTheXIII <24420859+KTheXIII@users.noreply.github.com>
2019-11-21 21:39:37 +0100

    moved the futnctions and added power sensor and more data is sent

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,92 @@
+#include <Adafruit_INA219.h>
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
 #include <SHT3x.h>
 #include <WiFi.h>
+#include <Wire.h>
 
 #define uS_TO_S_FACTOR 1000000 // Conversion factor for micro seconds to seconds
 #define TIME_TO_SLEEP 300      // Time ESP32 will go to sleep (in seconds)
 #define ERROR_SLEEP 5
 
-const int batPin = A13;
-
 const char *ssid = "SSID";
 const char *password = "PASSWORD";
 const String API_LINK = "APILINK";
+
 SHT3x thSensor;
+Adafruit_INA219 ina219;
 
-uint8_t temprature_sens_read();
-
-float voltage(0.0);
 bool err(false), onOff(false);
 unsigned long lastMillis(0), lastMillis2(0);
 int errcounter(0);
 
+float totalCurrent(0.0);
+int measuredCQ(0);
+
+void sendMessage();
+void errors();
+
+void setup() {
+  Serial.begin(115200);
+
+  thSensor.Begin();
+  ina219.begin();
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
+  delay(4000); // Delay needed before calling the WiFi.begin
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) { // Check for the connection
+    delay(250);
+    Serial.print(".");
+    errcounter++;
+    errors();
+  }
+  Serial.println();
+  Serial.println("Connected to the WiFi network");
+
+  sendMessage();
+
+  if (!err)
+    esp_deep_sleep_start();
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  if (err && millis() - lastMillis2 >= 2500) {
+    lastMillis2 = millis();
+    sendMessage();
+
+    if (!err)
+      esp_deep_sleep_start();
+  } else if (millis() - lastMillis >= 500) {
+    lastMillis = millis();
+    digitalWrite(13, onOff);
+    onOff = !onOff;
+    errcounter++;
+  }
+
+  errors();
+}
+
 void sendMessage() {
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float currentmA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
+
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  currentmA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+
+  int batt = map((long)(busvoltage * 100), 320, 420, 0, 100);
   thSensor.UpdateData();
-  int analogValue = analogRead(batPin);
-  voltage = analogValue * 2 * (3.3 / 4095.0) * 1.082;
-  int batt = constrain(map((int)(voltage * 100), 320, 420, 0, 100), 0, 100);
 
   if (WiFi.status() == WL_CONNECTED) { // Check WiFi connection status
     StaticJsonDocument<500> doc;
@@ -34,8 +94,12 @@ void sendMessage() {
     doc["type"] = "TH";
     doc["temp"] = thSensor.GetTemperature();
     doc["hum"] = thSensor.GetRelHumidity();
+
     doc["batt"] = batt;
-    doc["voltage"] = voltage;
+    doc["voltage"] = busvoltage;
+    doc["current_mA"] = currentmA;
+    doc["power_mW"] = power_mW;
+    doc["loadvoltage"] = loadvoltage;
 
     HTTPClient http;
     http.begin(API_LINK);
@@ -74,45 +138,4 @@ void errors() {
     esp_sleep_enable_timer_wakeup(ERROR_SLEEP * uS_TO_S_FACTOR);
     esp_deep_sleep_start();
   }
-}
-
-void setup() {
-  Serial.begin(115200);
-  thSensor.Begin();
-
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-
-  delay(4000); // Delay needed before calling the WiFi.begin
-  WiFi.begin(ssid, password);
-
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) { // Check for the connection
-    delay(250);
-    Serial.print(".");
-    errcounter++;
-    errors();
-  }
-  Serial.println();
-  Serial.println("Connected to the WiFi network");
-
-  sendMessage();
-  if (!err)
-    esp_deep_sleep_start();
-  pinMode(13, OUTPUT);
-}
-
-void loop() {
-  if (err && millis() - lastMillis2 >= 2500) {
-    lastMillis2 = millis();
-    sendMessage();
-    if (!err)
-      esp_deep_sleep_start();
-  } else if (millis() - lastMillis >= 250) {
-    lastMillis = millis();
-    digitalWrite(13, onOff);
-    onOff = !onOff;
-    errcounter++;
-  }
-
-  errors();
 }
